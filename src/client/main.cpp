e8a84222be4531d206b1d2278a833379b9b18e41
AliZhr <ali.zouhairi@laposte.net>
2022-10-25 16:03:16 +0200

    tet

---
+++ src/client/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
+#include <SFML/Graphics.hpp>
+
+void testSFML() {
+    sf::Texture texture;
+}
+
+// Fin test SFML
+
+#include <state.hpp>
+
+using namespace std;
+using namespace state;
+
+int main(int argc,char* argv[])
+{
+    if ((string)argv[argc-1]=="hello"){
+        cout << "Hello World !" << endl;
+    }
+    else {
+        cout << "It works !" << endl;
+    }
+
+
+    return 0;
+}
